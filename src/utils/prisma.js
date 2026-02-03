f0062442febc94160f82ff71209b573d4491a14a
aafaaqamir <100368436+aafaaqamir@users.noreply.github.com>
2026-02-03 22:19:45 +0500

    Update prisma.js

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -2,13 +2,12 @@ const { PrismaClient } = require("@prisma/client");
 
 let prisma;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+if (!global.prisma) {
+  global.prisma = new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
 }
 
-module.exports = prisma;
+prisma = global.prisma;
+
+export default prisma;
